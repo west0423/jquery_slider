10b1f2b3b02bdaf1a6fd19f97a037be3bcbfa19b
west0423 <167289395+west0423@users.noreply.github.com>
2024-06-05 15:52:52 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// JavaScript
+//console.log('Hello world!');
+
+$(function(){
+  $('.bxslider').bxSlider({
+    mode: 'horizontal',
+      captions: true,
+      auto:true,
+      speed:500,
+      pause:2000,
+      autohover:false,
+      pager:false,
+  });
+});
